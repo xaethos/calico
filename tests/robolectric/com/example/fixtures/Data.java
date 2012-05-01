5163d826a9ab6c761937ca269b13244899885e2a
Diego Garcia <diego@xaethos.net>
2012-05-02 00:01:38 +0800

    Implements ActiveProvider#insert
    [#28666243]

--- tests/robolectric/com/example/fixtures/Data.java
+++ tests/robolectric/com/example/fixtures/Data.java
@@ -15,7 +15,15 @@ public interface Data {
     public static final String ID         = BaseColumns._ID;
     public static final String FOO        = "foo";
     public static final String BAR        = "bar";
+    public static final String BOOL       = "bool";
+    public static final String SHORT      = "short";
+    public static final String INT        = "int";
+    public static final String LONG       = "long";
+    public static final String FLOAT      = "float";
+    public static final String DOUBLE     = "double";
+    public static final String DATA       = "data";
     public static final String CREATED_AT = "created_at";
+    public static final String UPDATED_AT = "updated_at";
 
     @Getter(ID) public Long getId();
 
@@ -25,5 +33,27 @@ public interface Data {
     @Getter(BAR) public String getBar();
     @Setter(BAR) public String setBar();
 
+    @Getter(BOOL) public Boolean getBool();
+    @Setter(BOOL) public Boolean setBool();
+
+    @Getter(SHORT) public Short getShort();
+    @Setter(SHORT) public Short setShort();
+
+    @Getter(INT) public Integer getInt();
+    @Setter(INT) public Integer setInt();
+
+    @Getter(LONG) public Long getLong();
+    @Setter(LONG) public Long setLong();
+
+    @Getter(FLOAT) public Float getFloat();
+    @Setter(FLOAT) public Float setFloat();
+
+    @Getter(DOUBLE) public Double getDouble();
+    @Setter(DOUBLE) public Double setDouble();
+
+    @Getter(DATA) public byte[] getData();
+    @Setter(DATA) public byte[] setData();
+
     @Getter(CREATED_AT) public Long getCreatedAt();
+    @Getter(UPDATED_AT) public Long getUpdatedAt();
 }
--- tests/robolectric/net/xaethos/lib/activeprovider/content/ActiveProviderTest.java
+++ tests/robolectric/net/xaethos/lib/activeprovider/content/ActiveProviderTest.java
@@ -1,5 +1,6 @@
 package net.xaethos.lib.activeprovider.content;
 
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.database.Cursor;
@@ -9,20 +10,25 @@ import android.net.Uri;
 import android.provider.BaseColumns;
 import com.example.fixtures.Data;
 import com.example.fixtures.DataProvider;
+import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
+import com.xtremelabs.robolectric.shadows.ShadowContentResolver;
 import com.xtremelabs.robolectric.shadows.ShadowSQLiteCursor;
 import net.xaethos.lib.activeprovider.annotations.Model;
 import net.xaethos.lib.activeprovider.annotations.Provider;
+import net.xaethos.lib.activeprovider.models.ModelManager;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import static com.xtremelabs.robolectric.Robolectric.shadowOf;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.junit.Assert.assertThat;
 
@@ -33,8 +39,8 @@ public class ActiveProviderTest {
 
 	DataProvider provider;
 
-//	ContentResolver resolver;
-//	ShadowContentResolver resolverShadow;
+	ContentResolver resolver;
+	ShadowContentResolver resolverShadow;
 
     Model dataInfo;
     Uri dirUri;
@@ -58,10 +64,10 @@ public class ActiveProviderTest {
         itemUri = Uri.parse("content://" + authority + "/" + tableName + "/1");
     }
 
-//	@Before public void getResolver() {
-//		resolver = Robolectric.application.getContentResolver();
-//		resolverShadow = Robolectric.shadowOf(resolver);
-//	}
+	@Before public void getResolver() {
+		resolver = Robolectric.application.getContentResolver();
+		resolverShadow = shadowOf(resolver);
+	}
 
 	/////////////// Tests ///////////////
 
@@ -92,8 +98,20 @@ public class ActiveProviderTest {
         Cursor cursor;
 
         cursor = provider.query(dirUri, null, null, null, null);
-        assertThat(cursor.getColumnCount(), is(4));
-        assertThat(Arrays.asList(cursor.getColumnNames()), hasItems("_id", "foo", "bar", "created_at"));
+        assertThat(cursor.getColumnCount(), is(12));
+        assertThat(Arrays.asList(cursor.getColumnNames()), hasItems(
+                "_id",
+                "foo",
+                "bar",
+                "bool",
+                "short",
+                "int",
+                "long",
+                "float",
+                "double",
+                "data",
+                "created_at",
+                "updated_at"));
 
         cursor = provider.query(dirUri, ID_PROJECTION, null, null, null);
         assertThat(cursor.getColumnCount(), is(1));
@@ -149,53 +167,53 @@ public class ActiveProviderTest {
 		assertThat(cursor.getLong(0), is(idBarFirst));
 	}
 
-//	@Test public void canInsert() {
-//		ContentValues values = new ContentValues();
-//		values.put("pBoolean", true);
-//		values.put("pByte", (byte)16);
-//		values.put("pBlob", new byte[]{0,1,2,3});
-//		values.put("pDouble", 8.8);
-//		values.put("pFloat", 4.4F);
-//		values.put("pInteger", 512);
-//		values.put("pLong", 1234567890L);
-//		values.put("pShort", (short)42);
-//		values.put("pString", "Hello World!");
-//
-//		Uri uri = provider.insert(mConcreteRecord.getContentUri(), values);
-//		assertThat(provider.getType(uri), is(mConcreteRecord.getContentItemType()));
-//
-//		Cursor cursor = provider.query(uri, null, null, null, null);
-//		assertThat(cursor.moveToFirst(), is(true));
-//		assertThat(cursor.getInt(cursor.getColumnIndex("pBoolean")), is(not(0)));
-//		assertThat((byte)cursor.getShort(cursor.getColumnIndex("pByte")), is((byte)16));
-//		assertThat(cursor.getBlob(cursor.getColumnIndex("pBlob")), is(new byte[]{0,1,2,3}));
-//		assertThat(cursor.getDouble(cursor.getColumnIndex("pDouble")), is(8.8));
-//		assertThat(cursor.getFloat(cursor.getColumnIndex("pFloat")), is(4.4F));
-//		assertThat(cursor.getInt(cursor.getColumnIndex("pInteger")), is(512));
-//		assertThat(cursor.getLong(cursor.getColumnIndex("pLong")), is(1234567890L));
-//		assertThat(cursor.getShort(cursor.getColumnIndex("pShort")), is((short)42));
-//		assertThat(cursor.getString(cursor.getColumnIndex("pString")), is("Hello World!"));
-//		assertThat(cursor.isNull(cursor.getColumnIndex("pParent")), is(true));
-//	}
-//
-//	@Test(expected=IllegalArgumentException.class)
-//	public void insertHandlesItemUris() {
-//		ContentValues values = new ContentValues();
-//		values.put("foo", "Hello");
-//		Uri uri = Uri.withAppendedPath(dirUri, "1");
-//		provider.insert(uri, values);
-//	}
-//
-//	@Test public void insertNotifiesUri() {
-//		ContentValues values = new ContentValues();
-//		values.put("foo", "Hello");
-//
-//		resolverShadow.getNotifiedUris().clear();
-//		provider.insert(dirUri, values);
-//		assertThat(resolverShadow.getNotifiedUris().size(), is(1));
-//		assertThat(resolverShadow.getNotifiedUris(), hasItems(dirUri));
-//	}
-//
+	@Test public void canInsert() {
+		ContentValues values = new ContentValues();
+		values.put("bool", true);
+		values.put("data", new byte[]{0,1,2,3});
+		values.put("double", 8.8);
+		values.put("float", 4.4F);
+		values.put("int", 512);
+		values.put("long", 1234567890L);
+		values.put("short", (short)42);
+        values.put("foo", "Hello World!");
+        values.putNull("bar");
+
+        Uri uri = provider.insert(ModelManager.getContentUri(Data.class), values);
+		assertThat(provider.getType(uri), is(ModelManager.getContentItemType(Data.class)));
+
+		Cursor cursor = provider.query(uri, null, null, null, null);
+		assertThat(cursor.moveToFirst(), is(true));
+		assertThat(cursor.getInt(cursor.getColumnIndex("bool")), is(not(0)));
+		assertThat(cursor.getBlob(cursor.getColumnIndex("data")), is(new byte[]{0,1,2,3}));
+		assertThat(cursor.getDouble(cursor.getColumnIndex("double")), is(8.8));
+		assertThat(cursor.getFloat(cursor.getColumnIndex("float")), is(4.4F));
+		assertThat(cursor.getInt(cursor.getColumnIndex("int")), is(512));
+		assertThat(cursor.getLong(cursor.getColumnIndex("long")), is(1234567890L));
+		assertThat(cursor.getShort(cursor.getColumnIndex("short")), is((short)42));
+		assertThat(cursor.getString(cursor.getColumnIndex("foo")), is("Hello World!"));
+		assertThat(cursor.isNull(cursor.getColumnIndex("bar")), is(true));
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void insertHandlesItemUris() {
+		ContentValues values = new ContentValues();
+		values.put("foo", "Hello");
+		Uri uri = Uri.withAppendedPath(dirUri, "1");
+		provider.insert(uri, values);
+	}
+
+	@Test public void insertNotifiesUri() {
+		ContentValues values = new ContentValues();
+		values.put("foo", "Hello");
+
+		resolverShadow.getNotifiedUris().clear();
+		provider.insert(dirUri, values);
+        List<ShadowContentResolver.NotifiedUri> notifiedUris = resolverShadow.getNotifiedUris();
+		assertThat(notifiedUris.size(), is(1));
+		assertThat(notifiedUris.get(0).uri, is(dirUri));
+	}
+
 //	@Test public void canUpdate() {
 //		ContentValues values = new ContentValues();
 //		values.put("foo", "Hello");
@@ -329,7 +347,19 @@ public class ActiveProviderTest {
 
     public void prepareDatabase() {
         SQLiteDatabase database = provider.mDBHelper.getWritableDatabase();
-        database.execSQL("CREATE TABLE data (_id INTEGER PRIMARY KEY, foo TEXT, bar TEXT, created_at INTEGER);");
+        database.execSQL("CREATE TABLE data (" +
+                "_id INTEGER PRIMARY KEY," +
+                "foo TEXT," +
+                "bar TEXT," +
+                "bool INTEGER," +
+                "short INTEGER," +
+                "int INTEGER," +
+                "long INTEGER," +
+                "float REAL," +
+                "double REAL," +
+                "data BLOB," +
+                "created_at INTEGER," +
+                "updated_at INTEGER);");
     }
 
 	private long makeSimpleData(ContentValues values) {
